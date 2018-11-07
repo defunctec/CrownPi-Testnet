8fe016d6f5f7ca3c6ac807f49697f354e60f07f0
defunctec <defuncteconomics@gmail.com>
2018-11-07 18:36:00 +0000

    Change
    fix

--- crownpiscript.sh
+++ crownpiscript.sh
@@ -65,6 +65,7 @@ configure_firewall() {
     sudo ufw allow ssh/tcp
     sudo ufw limit ssh/tcp
     sudo ufw allow 19340/tcp
+    sudo ufw allow 10050
     sudo ufw allow 53/udp
     sudo ufw allow 111/udp
     sudo ufw allow 123/udp
