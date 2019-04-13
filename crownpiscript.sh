ce426d4018c52aa12fb46b4a25d25debab50fd3c
defunctec <defuncteconomics@gmail.com>
2019-04-13 19:54:08 +0100

    New version
    Version update 0.13.2

--- crownpiscript.sh
+++ crownpiscript.sh
@@ -40,7 +40,7 @@ update_repos() {
 # Download Crown client (Update link with new client)
 download_package() {
     # Password change prompt
-    echo Getting 0.13.0 MN-PoS testnet client...
+    echo Getting 0.13.2 MN-PoS testnet client...
     # Create temporary directory
     dir=`mktemp -d`
     if [ -z "$dir" ]; then
@@ -49,7 +49,7 @@ download_package() {
         mkdir -p $dir
     fi
     # Change this later to take latest release version.
-    sudo wget "https://github.com/Crowndev/crown-core/releases/download/v0.13.0/Crown-0.13.0.0-RaspberryPi.zip" -O $dir/crown.zip
+    sudo wget "https://gitlab.crown.tech/crown/crown-core/-/jobs/5912/artifacts/download" -O $dir/crown.zip
 }
 
 # Install Crown client
